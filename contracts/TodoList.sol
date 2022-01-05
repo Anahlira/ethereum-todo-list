6e5d9bac342df31728745ec627b9780d12e2c433
unknown <morska_kotka1@abv.bg>
2022-01-05 11:59:53 +0200

    added src files

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,22 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract TodoList{
     uint public taskCount = 0; // this is variable for the whole file
+
+    struct Task {
+        uint id; //unit is unssigned int
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks; // key => the struct that we defined
+
+    // here you don't need to say it to be public, but it doesn't want to work without it... what
+    constructor() public {  
+        createTask("Don't forget to feed the cat");
+    }
+
+    function createTask(string memory _content) public{
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
--- truffle-config.js
+++ truffle-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 7545,
+      host: "127.0.0.1",//this is the local host
+      port: 7545, //here runs ganache
       network_id: "*" // Match any network id
     }
   },
